33482688097027059f8e8275ff7c81d7d7036bd8
vitor00almei <pondebatata@outlook.com>
2020-05-21 15:54:20 -0300

    daisy: Improve HWUI
    Enable some prop hw and surfaceflinger to improve HWUI

--- prop.mk
+++ prop.mk
@@ -119,6 +119,7 @@ persist.hwc.enable_vds=1 \
 persist.hwc.mdpcomp.enable=true \
 ro.opengles.version=196610 \
 ro.qualcomm.cabl=0 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
 ro.sf.lcd_density=420 \
 ro.vendor.display.cabl=2 \
 sdm.debug.disable_skip_validate=1 \
@@ -247,7 +248,9 @@ ro.surface_flinger.protected_contents=true \
 debug.sf.early_phase_offset_ns=1500000 \
 debug.sf.early_app_phase_offset_ns=1500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=4096
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
