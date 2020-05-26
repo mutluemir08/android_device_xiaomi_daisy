7260897b22f85c244c10c1b66ec7afc54108acec
vitor00almei <pondebatata@outlook.com>
2020-05-26 08:52:42 -0300

    daisy: prop: rework prop

--- prop.mk
+++ prop.mk
@@ -103,30 +103,16 @@ persist.dirac.poolsize=3
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
-debug.egl.hw=0 \
-debug.enable.sglscale=1 \
-debug.gralloc.enable_fb_ubwc=1 \
-debug.mdpcomp.logs=0 \
+debug.hwui.use_buffer_age=false \
 debug.sf.enable_hwc_vds=1 \
-debug.sf.hw=0 \
-debug.sf.latch_unsignaled=1 \
-debug.cpurend.vsync=false \
-debug.sf.recomputecrop=0 \
-dev.pm.dyn_samplingrate=1 \
-persist.debug.wfd.enable=1 \
-persist.demo.hdmirotationlock=false \
-persist.hwc.enable_vds=1 \
-persist.hwc.mdpcomp.enable=true \
+debug.sf.hw=1 \
 ro.opengles.version=196610 \
-ro.qualcomm.cabl=0 \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-ro.sf.lcd_density=420 \
-ro.vendor.display.cabl=2 \
-sdm.debug.disable_skip_validate=1 \
+vendor.display.disable_partial_split=1 \
+vendor.display.disable_rotator_downscale=1 \
 vendor.display.disable_skip_validate=1 \
-vendor.display.enable_default_color_mode=1 \
-vendor.gralloc.enable_fb_ubwc=1 \
-debug.hwui.renderer=opengl
+vendor.display.enable_default_color_mode=0 \
+vendor.display.perf_hint_window=50 \
+vendor.gralloc.enable_fb_ubwc=1
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -148,8 +134,7 @@ ro.frp.pst=/dev/block/bootdevice/by-name/config
 PRODUCT_PROPERTY_OVERRIDES += \
 persist.gps.qc_nlp_in_use=1 \
 persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1 \
-ro.ril.def.agps.mode=1
+ro.gps.agps_provider=1
 
 # IOP properties
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -243,14 +228,31 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ro.sensor.proximity=true
 
 # SurfaceFlinger
-PRODUCT_PROPERTY_OVERRIDES += \
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 ro.surface_flinger.protected_contents=true \
+ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.max_virtual_display_dimension=4096 \
+ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
+ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
+ro.surface_flinger.max_frame_buffer_acquired_buffers=3
+
+PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.early_phase_offset_ns=1500000 \
 debug.sf.early_app_phase_offset_ns=1500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000 \
-ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096
+    debug.sf.early_gl_app_phase_offset_ns=15000000 \
+    debug.sf.latch_unsignaled=1 \
+    debug.sf.disable_backpressure=1 \
+    debug.sf.enable_gl_backpressure=1
+
+# The default sf phase offset is set to 6ms, to avoid it be included into next
+# vsync threshold, set high fps early sf and next vsync threshold phase offset
+# to 6.1ms, which is bigger than all sf phase offsets in normal frame rate.
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.high_fps_early_phase_offset_ns=6100000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=9000000 \
+    debug.sf.phase_offset_threshold_for_next_vsync_ns=6100000
 
 # Time Services
 PRODUCT_PROPERTY_OVERRIDES += \
